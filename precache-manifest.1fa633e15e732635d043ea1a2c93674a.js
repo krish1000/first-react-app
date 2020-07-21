deed6dff4accd57039355e97f343153e9804aa34
Krishaanth <krish100@my.yorku.ca>
2020-07-21 17:50:57 -0400

    Updates

---
+++ precache-manifest.1fa633e15e732635d043ea1a2c93674a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f218dd89523b00b68869cef97ddd4bc7",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "1a79c9af6b1f01a2a2e9",
+    "url": "/first-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "910e6ba095098c91fda5",
+    "url": "/first-react-app/static/js/2.4f77c47f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.4f77c47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a79c9af6b1f01a2a2e9",
+    "url": "/first-react-app/static/js/main.8895f3e9.chunk.js"
+  },
+  {
+    "revision": "05e274721008cf2c5798",
+    "url": "/first-react-app/static/js/runtime-main.d82f5a8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
