cbcfb80df25fa3898eade684ffe4b67d0d621c38
Krishaanth <krish100@my.yorku.ca>
2020-07-23 00:21:38 -0400

    Hashrouter works! Browser router is not supported by ghpages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import { Home } from "./Components/Home";
+import { About } from "./Components/About";
+import { Contact } from "./Components/Contact"; //add the curly brackets around home, about, contact b/c there is no default export stated
 
+function App() {
+  return (
+    <div className="border1">
+      <div className="App">
+        <header className="App-header">
+          <img
+            src="https://i.imgur.com/6Nn4ucD.gif"
+            className="App-logo"
+            alt="logo"
+          />
+          <p>
+            Learning 2 deploy on ghpages <code>using react</code> DOES IT SPIN
+            REEEEEE.
+          </p>
+        </header>
+      </div>
+
+      <div className="blank">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={() => <div>404 Not found reeee</div>} />
+        </Switch>
+      </div>
+    </div>
+  );
+}
+export default App;
+
+/* ORIGINAL
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> }
+        {/* This is a comment, checking out jsx comments }
+        <img
+          src="https://i.imgur.com/6Nn4ucD.gif"
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/yawyeeet.js</code> and save poop very epic.
         </p>
@@ -24,3 +65,4 @@ function App() {
 }
 
 export default App;
+*/
---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// export default function Home() {
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
+
+export const Home = () => (
+  <div>
+    <h1> Home page</h1>
+    <p> Updated with PUBLIC_URL</p>
+  </div>
+);
+//using the curve bracket tricks it into thinking everything inside is in 1 return statement, so no need to use the above commented out code
